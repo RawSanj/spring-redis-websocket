d4109e57682db41ebaa2c6896a00d7d622f1d3ef
Sanjay Rawat <13738294+RawSanj@users.noreply.github.com>
2021-01-01 20:55:16 +0530

    Fixed Test to Stop Redis Container after test completion
    Also updated config to decouple native-image generation with docker image to be able to run each task as seperate maven build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Runs Tests and Creates Graal Native Image - Uses TestContainers to start the Redis Container required for Test
-mvn -Pnative clean install
+# Runs Tests and Creates Graal Native Image - Uses TestContainers to start the Redis Container required for Test (Docker is required)
+mvn -Pnative clean package -DskipNativeImage=false
 
-# Runs Tests and Creates Graal Native Docker Image - Uses TestContainers to start the Redis Container required for Test
+# Runs Tests and Creates Graal Native Docker Image - Uses TestContainers to start the Redis Container required for Test (Docker is required)
 mvn -Pnative clean spring-boot:build-image
 
-# Runs Tests and Creates Docker Image - Uses TestContainers to start the Redis Container required for Test
+# Creates Docker Image - Skips Integration Tests and hence Docker is not required
 mvn clean spring-boot:build-image
